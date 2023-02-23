d67c224586aa41277c27bf1d001f14786bcfecca
ArisaBonsaiTree <angelevarela@hotmail.com>
2023-02-23 10:37:12 -0800

    Just implemented ClassRosterDaoException

---
+++ src/com/av/dvdlibrary/dao/DvdLibraryDaoException.java
@@ -0,0 +1,12 @@
+package com.av.dvdlibrary.dao;
+
+public class DvdLibraryDaoException extends Exception{
+
+    public DvdLibraryDaoException(String message) {
+        super(message);
+    }
+
+    public DvdLibraryDaoException(String message, Throwable cause){
+        super(message, cause);
+    }
+}
